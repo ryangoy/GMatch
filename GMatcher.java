2e7ac283c9e9672c3c63ed5750d3b27d2bcfd345
ryangoy <ryangoy@gmail.com>
2015-04-13 14:44:15 -0700

    compiles and wrote outline for algorithm

--- GMatcher.java
+++ GMatcher.java
@@ -1,13 +1,18 @@
 import java.util.*;
 //Input group members and preferences and output groups
 public class GMatcher {
-	int numChoices, numPeople, groupSize;
+	int numChoices, numPeople, groupSize, numGroups;
 	HashMap<String, HashSet<String>> data;
 
 	public GMatcher(int numChoices, int numPeople, int groupSize) {
 		this.numChoices = numChoices;
 		this.numPeople = numPeople;
 		this.groupSize = groupSize;
+		if (numPeople % groupSize == 0) {
+			this.numGroups = numPeople / groupSize;
+		} else {
+			this.numGroups = numPeople / groupSize + 1;
+		}
 		HashMap<String, HashSet<String>> data = new HashMap<String, HashSet<String>>();
 	}
 
@@ -16,6 +21,25 @@ public class GMatcher {
 	}
 
 	public HashMap<Integer, HashSet<String>> computeGroups() {
+		//1) create data structure
+
+		//2) populate pgroups (randomly)
+
+		//3) loop through crossing over pgroups. of the three per crossover (parent, parent, child), 
+		//		97/100 chance: calculate fitness and delete the least fit node
+		//		2/100 chance: randomly select 2 to avoid a local max
+		//		1/100 return a randomly mutated child
+
+		//4) find the most fit node after n iterations, and repopulate based on this node by permutating psets
+
+		//5) repeat crossover/mutation and bottleneck m times (i.e. go back to step 3)
+
+		//6) optional: calculate fitness efficiency score: (calculated fitness) / (possible matches)
+
+		//change the orientation of pgroups
+
+		//create groups from the pgroups
+
 		return null;
 	}
 }
