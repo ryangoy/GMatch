2a7013647e006dfb2fad7d1dbe342452c7dbb6d0
ryangoy <ryangoy@gmail.com>
2015-04-13 14:31:17 -0700

    testing ui done

--- GMatcher.java
+++ GMatcher.java
@@ -1,7 +1,21 @@
-
+import java.util.*;
 //Input group members and preferences and output groups
 public class GMatcher {
-	public GMatcher() {
-		
+	int numChoices, numPeople, groupSize;
+	HashMap<String, HashSet<String>> data;
+
+	public GMatcher(int numChoices, int numPeople, int groupSize) {
+		this.numChoices = numChoices;
+		this.numPeople = numPeople;
+		this.groupSize = groupSize;
+		HashMap<String, HashSet<String>> data = new HashMap<String, HashSet<String>>();
+	}
+
+	public void putData(HashMap<String, HashSet<String>> data) {
+		this.data = data;
+	}
+
+	public HashMap<Integer, HashSet<String>> computeGroups() {
+		return null;
 	}
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+//@author Ryan Goy 
 
 //Runs GMatcher
 public class Main {
@@ -8,7 +11,7 @@ public class Main {
 		
 		System.out.println("Welcome to GMatch.");
 		while(true) {
-			System.out.println("Type 'start' for a new dataset or type 'exit' to leave the program.");
+			System.out.println("Type (start) for a new dataset or type (exit) to leave the program.");
 			Scanner scan = new Scanner(System.in);
 			String input = scan.next();
 			switch(input) {
@@ -16,33 +19,12 @@ public class Main {
 					return;
 				case("start"):
 					getPreferences();
-					addPeople(null, numPeople);
+					GMatcher gm = new GMatcher(numChoices, numPeople, groupSize);
+					addPeople(gm, numPeople);
+					HashMap<Integer, HashSet<String>> groups = gm.computeGroups();
+					printGroups(groups);
 					break;
 			} //end switch
-		}
-	}
-
-	private static void addPeople(GMatcher g, int n) {
-		ArrayList<String> lst = new ArrayList<String>();
-		for (int i = 0; i < n; i++) {
-			System.out.print("Please enter a name: ");
-			Scanner scan = new Scanner(System.in);
-			String input = scan.next();
-			lst.add(input);
-		}
-
-		for (String s : lst) {
-			String pref = "";
-			for (int i = 1; i <= numChoices; i++) {
-				System.out.printf("Enter preference %d for %s: ", i, s);				
-				do {
-					Scanner scan = new Scanner(System.in);
-					pref = scan.next();
-					if (!lst.contains(pref)) {
-						System.out.println("That name is not in the set. Check case and spelling.");
-					}
-				} while (!lst.contains(pref));
-			}
 		}
 	}
 
@@ -60,4 +42,66 @@ public class Main {
 		numChoices = scan.nextInt();
 	}
 
+	private static void addPeople(GMatcher g, int n) {
+		HashMap<String, HashSet<String>> lst = new HashMap<String, HashSet<String>>();
+		//add Names
+		for (int i = 0; i < n; i++) {
+			String input = "";
+			do {
+				System.out.print("Please enter a name (no duplicates): ");
+				Scanner scan = new Scanner(System.in);
+				input = scan.next();
+				if (lst.containsKey(input)) {
+					System.out.println("That name already exists.");
+				}
+			} while(lst.containsKey(input));
+			lst.put(input, new HashSet<String>());
+		}
+
+		//add their preferences
+		System.out.println("Enter ther preferences for each person in order of weight.");
+		for (String s : lst.keySet()) {
+			HashSet<String> prefLst = lst.get(s);
+			String pref = "";
+			for (int i = 1; i <= numChoices; i++) {
+				System.out.printf("Enter preference %d for %s: ", i, s);				
+				do {
+					Scanner scan = new Scanner(System.in);
+					pref = scan.next();
+					if (!lst.containsKey(pref)) {
+						System.out.println("That name is not in the set. Check case and spelling.");
+					}
+				} while (!lst.containsKey(pref));
+				prefLst.add(pref);
+			}
+		}
+
+		//output the given dataset
+		confirmCorrect(lst);
+
+		//add this dataset to the GroupMatcher
+		g.putData(lst);
+	}
+
+	private static void confirmCorrect(HashMap<String, HashSet<String>> lst) {
+		System.out.println("Your dataset is as follows:");
+		for (String s : lst.keySet()) {
+			System.out.print(s + ": ");
+			for (String g : lst.get(s)) {
+				System.out.print(g + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	private static void printGroups(HashMap<Integer, HashSet<String>> groups) {
+		for (int i : groups.keySet()) {
+			System.out.printf("Group %d:\n", i);
+			for (String person : groups.get(i)) {
+				System.out.println(person);
+			}
+			System.out.println();
+		}
+		System.out.print("Thank you for using GMatcher. ");
+	}
 }
